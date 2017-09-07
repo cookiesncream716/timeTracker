6d99a96909e26d608f9ad356f2595eac6a992962
Heather Walker <cookiesncream@ladle.net>
2017-09-07 13:57:33 -0700

    switched to new plugin tester

--- tTracker.js
+++ tTracker.js
@@ -10,89 +10,98 @@ var Button = require('gem/Button')
 var Style = require('gem/Style')
 
 registerPlugin(proto(Gem, function(){
-	
 	this.name = 'TimeTracker'
+
+	// set ticket field options
+	// this.initialize = function(options){
+	// 	return{
+	// 		checkInField: 'checkIn',
+	// 		checkOutField: 'checkOut',
+	// 		timeWorkedField: 'timeWorked',
+	// 		userField: 'user
+	// 	}
+	// }
+
 	this.build = function(ticket, optionsObservee, api){
-		this.add(Text('hi'))
+		var that = this
+		this.ticket = ticket
+
+		// get access if there is already a check-in time
+		var inProperty = optionsObservee.subject.checkInField
+		// var inProperty = optionsObservee.checkInField
+		// var start = ticket.get(inProperty).subject
+
+		// get the current user
+		api.User.current().then(function(user){
+			that.currentUser = user.subject._id
+			console.log('user=' + that.currentUser)
+		}).done()
+
+		// Testing options
+		console.log('inProperty' + ticket.get(inProperty).subject)
+		console.log('out=' + ticket.get(optionsObservee.subject.checkOutField).subject)
+		this.ticket.set('user', this.currentUser)
+		console.log('user=' + ticket.get(optionsObservee.subject.userField).subject)
+		console.log('worked=' + ticket.get(optionsObservee.subject.timeWorkedField).subject)
+
+		// Check-In Time
 		this.checkIn = TextField()
-		var fp = new flatpickr(this.checkIn.domNode, {
-			enableTime: true,
-			dateFormat: 'm-d-Y h:i K',
-			defaultDate: new Date()
-		})
-		this.add(this.checkIn)
-
-		// var that = this
-		// this.ticket = ticket
-
-		// // get access if there is already a check-in time
-		// var inProperty = optionsObservee.subject.checkInField
-		// // var start = ticket.get(inProperty).subject
-
-		// // get the current user
-		// api.User.current().then(function(user){
-		// 	that.currentUser = user.subject._id
-		// }).done()
-
-		// // Check-In Time
-		// this.checkIn = TextField()
-		// this.min
-		// if(ticket.get(inProperty).subject === undefined){
-		// 	this.checkIn.val = new Date()
-		// 	this.setIn()
-		// 	var fp_in = new flatpickr(this.checkIn.domNode, {
-		// 		enableTime: true,
-		// 		dateFormat: 'm-d-Y h:i K',
-		// 		defaultDate: new Date(),
-		// 		minuteIncrement: 1,
-		// 		onClose: function(){
-		// 			that.setIn()
-		// 		}
-		// 	})
-		// } else{
-		// 	this.checkIn.val = ticket.get(inProperty).subject
-		// 	this.setIn()
-		// 	var fp_in = new flatpickr(this.checkIn.domNode, {
-		// 		enableTime: true,
-		// 		dateFormat: 'm-d-Y h:i K',
-		// 		defaultDate: ticket.get(inProperty).subject,
-		// 		minuteIncrement: 1,
-		// 		onClose: function(){
-		// 			that.setIn()
-		// 		}
-		// 	})
-		// }
+		if(ticket.get(inProperty).subject === undefined){
+			this.checkIn.val = new Date()
+			this.setIn()
+			var fp_in = new flatpickr(this.checkIn.domNode, {
+				enableTime: true,
+				dateFormat: 'm-d-Y h:i K',
+				defaultDate: new Date(),
+				minuteIncrement: 1,
+				onClose: function(){
+					that.setIn()
+				}
+			})
+		} else{
+			this.checkIn.val = ticket.get(inProperty).subject
+			this.setIn()
+			var fp_in = new flatpickr(this.checkIn.domNode, {
+				enableTime: true,
+				dateFormat: 'm-d-Y h:i K',
+				defaultDate: ticket.get(inProperty).subject,
+				minuteIncrement: 1,
+				onClose: function(){
+					that.setIn()
+				}
+			})
+		}
 
 		// // Check-Out Time
-		// var errMessage = Text('error', 'x')
-		// errMessage.visible = false
-		// this.checkOut = TextField()
-		// var fp_out = new flatpickr(this.checkOut.domNode, {
-		// 	enableTime: true,
-		// 	dateFormat: 'm-d-Y h:i K',
-		// 	minuteIncrement: 1,
-		// 	maxDate: 'today',
-		// 	onClose: function(){
-		// 		var y = that.checkOut.val
-		// 		that.mOut = new Date(y).getTime()
-		// 		if(y == '' || that.mOut < that.mIn){
-		// 			errMessage.text = 'Your End Time must be later than your Start Time'
-		// 			errMessage.visible = true
-		// 			that.checkOut.val = ''
-		// 		} else{
-		// 			errMessage.visible = false
-		// 			that.calculateTime()
-		// 		}
-		// 	}
-		// })
+		var errMessage = Text('error', 'x')
+		errMessage.visible = false
+		this.checkOut = TextField()
+		var fp_out = new flatpickr(this.checkOut.domNode, {
+			enableTime: true,
+			dateFormat: 'm-d-Y h:i K',
+			minuteIncrement: 1,
+			maxDate: 'today',
+			onClose: function(){
+				var y = that.checkOut.val
+				that.mOut = new Date(y).getTime()
+				if(y == '' || that.mOut < that.mIn){
+					errMessage.text = 'Your End Time must be later than your Start Time'
+					errMessage.visible = true
+					that.checkOut.val = ''
+				} else{
+					errMessage.visible = false
+					that.calculateTime()
+				}
+			}
+		})
 
-		// this.workedText = Text()
-		// this.workedText.visible = false
-		// var box = Text('div', 'Keep track of the time you are working.')
-		// var box2 = Block('div', Text('Start Time: '), this.checkIn, Text(' End Time: '), this.checkOut, errMessage, this.workedText)
+		this.workedText = Text()
+		this.workedText.visible = false
+		var box = Text('div', 'Keep track of the time you are working.')
+		var box2 = Block('div', Text('Start Time: '), this.checkIn, Text(' End Time: '), this.checkOut, errMessage, this.workedText)
 		// box.visible = false
 		// box2.visible = false
-
+		this.add(box, box2)
 		// var button = Button('timeTracker')
 		// this.add(button, box, box2)
 		// button.on('click', function(){
@@ -101,37 +110,50 @@ registerPlugin(proto(Gem, function(){
 		// 	// does button need hidden?
 		// })
 
-		// // convert check-in time to milliseconds and save it
-		// this.setIn = function(){
-		// 	var ms = this.checkIn.val
-		// 	this.mIn = new Date(ms).getTime()
-		// 	this.ticket.set('checkIn', this.checkIn.val)
-		// }
+		// css for flatpickr
+		this.on('attach', function(){
+		var flatpickrStylesheet = require('raw-loader!flatpickr/dist/flatpickr.min.css')
+		var style = document.createElement('style')
+		style.innerHTML = flatpickrStylesheet
+		document.head.appendChild(style)
+		})
+	}
 
-		// // find how long worked and save out/time worked/user
-		// this.calculateTime = function(){
-		// 	var diff = this.mOut - this.mIn
-		// 	var hours = Math.floor(diff/1000/60/60)
-		// 	diff -= hours*1000*60*60
-		// 	var minutes = Math.floor(diff/1000/60)
-		// 	this.workedText.text = 'You worked ' + hours + ' hours and ' + minutes + ' minutes.'
-		// 	this.workedText.visible = true
-		// 	this.ticket.set('checkOut', this.checkOut.val)
-		// 	// timeWorked is in milliseconds so need to do math to convert to hours/minutes when displayed
-		// 	this.ticket.set('timeWorked', diff)
-		// 	this.ticket.set('user', this.currentUser)
-		// }
+	// convert check-in time to milliseconds and save it
+	this.setIn = function(){
+		var ms = this.checkIn.val
+		this.mIn = new Date(ms).getTime()
+		this.ticket.set('checkIn', this.checkIn.val)
+		console.log('mIn=' + this.mIn)
+		console.log(this.checkIn.val)
+	}
 
-		// this.getStyle = function(){
-		// 	return Style({
-		// 		$div: {
-		// 			display: 'block'
-		// 		},
-		// 		$error: {
-		// 			color: 'red',
-		// 			display: 'block'
-		// 		}
-		// 	})
-		// }
+	// // find how long worked and save out/time worked/user
+	this.calculateTime = function(){
+		var diff = this.mOut - this.mIn
+		var hours = Math.floor(diff/1000/60/60)
+		diff -= hours*1000*60*60
+		var minutes = Math.floor(diff/1000/60)
+		this.workedText.text = 'You worked ' + hours + ' hours and ' + minutes + ' minutes.'
+		this.workedText.visible = true
+		this.ticket.set('checkOut', this.checkOut.val)
+		// timeWorked is in milliseconds so need to do math to convert to hours/minutes when displayed
+		this.ticket.set('timeWorked', diff)
+		this.ticket.set('user', this.currentUser)
+		console.log('in=' + ticket.get(inProperty).subject)
+		console.log('out=' + ticket.get(optionsObservee.subject.checkOutField).subject)
+		console.log('user=' + ticket.get(optionsObservee.subject.userField).subject)
+		console.log('worked=' + ticket.get(optionsObservee.subject.timeWorkedField).subject)
+	}
+	this.getStyle = function(){
+		return Style({
+			$div: {
+				display: 'block'
+			},
+			$error: {
+				color: 'red',
+				display: 'block'
+			}
+		})
 	}
 }))
