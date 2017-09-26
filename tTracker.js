5880fc52be42bd9f5c3ed0ab90f130b45f215051
Heather Walker <cookiesncream@ladle.net>
2017-09-26 15:00:58 -0700

    refactoring

--- tTracker.js
+++ tTracker.js
@@ -3,6 +3,8 @@ var flatpickr = require('flatpickr')
 registerPlugin(proto(Gem, function(){
 	this.name = 'TimeTracker'
 
+
+
 	// set ticket field options 
 	this.initialize = function(options){
 		return{
@@ -16,19 +18,23 @@ registerPlugin(proto(Gem, function(){
 	this.build = function(ticket, optionsObservee, api){
 		var that = this
 		this.ticket = ticket
+		this.api = api
+		this.optionsObservee = optionsObservee
+		this.checkIn = TextField()
+		this.checkOut = TextField()
+		this.workedText = Text()
+		this.workedText.visible = false
+		var errMessage = Text('error', 'x')
+		errMessage.visible = false
+		var box = Block('div', Text('Start Time: '), this.checkIn, Text(' End Time: '), this.checkOut, errMessage, this.workedText)
+		this.add(box)
 
 		// get access if there is already a check-in time
 		var inProperty = optionsObservee.subject.checkInField
-
-		// get the current user
-		api.User.current().then(function(user){
-			that.currentUser = user.subject._id
-			console.log('user = ' + that.currentUser)
-		}).done()
+		this.getIn = ticket.get(inProperty)
 
 		// Check-In Time
-		this.checkIn = TextField()
-		if(ticket.get(inProperty).subject === undefined){
+		if(this.getIn.subject === undefined){
 			var fp_in = new flatpickr(this.checkIn.domNode, {
 				enableTime: true,
 				dateFormat: 'm-d-Y h:i K',
@@ -39,15 +45,12 @@ registerPlugin(proto(Gem, function(){
 				}
 			})
 		} else{
-			console.log('set to '+ ticket.get(inProperty).subject)
-			this.checkIn.val = ticket.get(inProperty).subject
-			this.setIn()
 			var fp_in = new flatpickr(this.checkIn.domNode, {
 				enableTime: true,
 				dateFormat: 'm-d-Y h:i K',
-				defaultDate: ticket.get(inProperty).subject,
+				defaultDate: this.getIn.subject,
 				minuteIncrement: 1,
-				// maxDate: 'today',
+				maxDate: 'today',
 				onClose: function(){
 					that.setIn()
 				}
@@ -55,90 +58,58 @@ registerPlugin(proto(Gem, function(){
 		}
 
 		// Check-Out Time
-		var errMessage = Text('error', 'x')
-		errMessage.visible = false
-		this.workedText = Text()
-		this.workedText.visible = false
-		this.checkOut = TextField()
 		var fp_out = new flatpickr(this.checkOut.domNode, {
 			enableTime: true,
 			dateFormat: 'm-d-Y h:i K',
 			minuteIncrement: 1,
 			maxDate: 'today',
 			onClose: function(){
-				var y = that.checkOut.val
-				that.mOut = new Date(y).getTime()
-				if(y == '' || that.mOut < that.mIn){
+				if(that.checkOut.val == '' || new Date(that.checkOut.val).getTime() < new Date(that.getIn.subject).getTime()){
 					errMessage.text = 'Your End Time must be later than your Start Time'
 					errMessage.visible = true
 					that.checkOut.val = ''
 				} else{
 					errMessage.visible = false
 					that.calculateTime()
-					// setTimeout(that.workedText.visible = true, 1000)
-					// that.workedText.visible = false
-					// setTimeout(function(){
-					// 	that.workedText.visible = true, 500
-					// })
-					// that.workedText.visible = false
 				}
 			}
 		})
 
-		var box = Block('div', Text('Start Time: '), this.checkIn, Text(' End Time: '), this.checkOut, errMessage, this.workedText)
-	// Don't think the buttons are needed on actual Tixit site
-		// var button = Button('timeTracker')
-		// var button2 = Button('close')
-		// button2.visible = false
-		// this.add(button, button2, box)
-		this.add(box)
-		// button.on('click', function(){
-		// 	// box.visible = true
-		// 	box2.visible = true
-		// 	button.visible = false
-		// 	button2.visible = true
-		// })
-		// button2.on('click', function(){
-		// 	box2.visible = false
-		// 	button.visible = true
-		// 	button2.visible = false
-		// })
-
 		// css stylesheet for flatpickr
 		this.on('attach', function(){
-		var flatpickrStylesheet = require('raw-loader!flatpickr/dist/flatpickr.min.css')
-		var style = document.createElement('style')
-		style.innerHTML = flatpickrStylesheet
-		document.head.appendChild(style)
+			var flatpickrStylesheet = require('raw-loader!flatpickr/dist/flatpickr.min.css')
+			var style = document.createElement('style')
+			style.innerHTML = flatpickrStylesheet
+			document.head.appendChild(style)
 		})
 	}
 
 	// convert check-in time to milliseconds and save it
 	this.setIn = function(){
-		var ms = this.checkIn.val
-		this.mIn = new Date(ms).getTime()
-		this.ticket.set('checkIn', new Date(this.checkIn.val))
-		console.log('mIn=' + this.mIn)
-		console.log(this.checkIn.val)
-		console.log(new Date(this.checkIn.val))
+		this.ticket.set(this.optionsObservee.subject.checkInField, new Date(this.checkIn.val))
 	}
 
-	// find how long worked and save out/time worked/user
+	// find how long worked and save out/timeWorked/user
 	this.calculateTime = function(){
-		// var that = this
-		var diff = this.mOut - this.mIn
-		var hours = Math.floor(diff/1000/60/60)
-		diff -= hours*1000*60*60
-		var minutes = Math.floor(diff/1000/60)
+		var that = this
+		var tWorked = new Date(that.checkOut.val).getTime() - new Date(this.getIn.subject).getTime()
+		var hours = Math.floor(tWorked/1000/60/60)
+		var rTime = tWorked - hours*1000*60*60
+		var minutes = Math.floor(rTime/1000/60)
 		this.workedText.text = 'You worked ' + hours + ' hours and ' + minutes + ' minutes.'
 		this.workedText.visible = true
 		// setTimeout(function(){
 		// 	that.workedText.visible = true
 		// }, 3000)
-		this.ticket.set('checkOut', new Date(this.checkOut.val))
+		this.ticket.set(this.optionsObservee.subject.checkOutField, new Date(this.checkOut.val))
 		// timeWorked is in milliseconds so need to do math to convert to hours/minutes when displayed
-		this.ticket.set('timeWorked', diff)
-		this.ticket.set('user', this.currentUser)
+		this.ticket.set(this.optionsObservee.subject.timeWorkedField, tWorked)
+		tWorked = 0
+		// get the current user
+		this.api.User.current().then(function(user){
+			that.currentUser = user.subject._id
+		}).done()
+		this.ticket.set(this.optionsObservee.subject.userField, this.currentUser)
 		this.checkIn.val = ''
 		this.checkOut.val = ''
 	}
